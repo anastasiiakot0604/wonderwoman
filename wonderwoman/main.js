8c54a42ec2bc90ead244037e7e86e87e471d7d85
Anastasiia <kotelnikova.anastasiia@gmail.com>
2019-07-28 11:26:29 +0200

    added portfolio

--- wonderwoman/main.js
+++ wonderwoman/main.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
     $('.header').height($(window).height());
   })
+
+  // MDB Lightbox Init
+$(function () {
+  $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+  });
